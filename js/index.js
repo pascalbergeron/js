b4eb3b50845b6cdcbb46355c49ea048e7c32db0f
Pascal-Alain Bergeron <pascal.alain.bergeron@gmail.com>
2017-06-23 02:31:48 -0400

    big changes
    bg, sketch #5, animation, cipher pour Sonia, CSS

--- js/index.js
+++ js/index.js
@@ -1,17 +1,47 @@
 
+// Original
+// var colors = new Array(
+  // [62,35,255],
+  // [60,255,60],
+  // [255,35,98],
+  // [45,175,230],
+  // [255,0,255],
+  // [255,128,0]);
+
+// Pale 1 [PAS UTILISÉ ENCORE]
+// var colors = new Array(
+  // [192,87,70],
+  // [173,198,152],
+  // [208,227,196],
+  // [255,255,255],
+  // [80,48,71],
+  // [120,133,133]);
+
+  // pale 2[PAS UTILISÉ ENCORE]
+// var colors = new Array(
+  // [242,84,91],
+  // [169,63,85],
+  // [25,50,60],
+  // [243,247,240],
+  // [140,94,88],
+  // [8,178,227]);
+
+  // couleur vive
 var colors = new Array(
-  [62,35,255],
-  [60,255,60],
-  [255,35,98],
-  [45,175,230],
-  [255,0,255],
-  [255,128,0]);
+  [237,27,0],
+  [168,0,33],
+  [0,32,61],
+  [0,177,226],
+  [136,12,244],
+  [245,187,0]);
   
 var step = 0;
 
-var colorIndices = [0,1,2,3];
+// var colorIndices = [0,1,2,3];
+var colorIndices = [0,2,1,3];
 
-var gradientSpeed = 0.004;
+// var gradientSpeed = 0.004;
+var gradientSpeed = 0.008;
 
 function updateGradient()
 {
@@ -35,7 +65,8 @@ var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
 var color2 = "rgb("+r2+","+g2+","+b2+")";
 
  $('#gradient').css({
-   background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
+    // background: "-webkit-gradient(linear, left bottom, rig ht top, from("+color1+"), to("+color2+"))"}).css({
+	background: "-webkit-gradient(radial, center center, 0, center center, 1401, from("+color1+"), color-stop(50%, "+color2+"), to("+color1+"))"}).css({
     background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
   
   step += gradientSpeed;
@@ -52,3 +83,29 @@ var color2 = "rgb("+r2+","+g2+","+b2+")";
 }
 
 setInterval(updateGradient,10);
+
+/////////////////////////////************ *****************/////////////////////
+// Mon anim
+
+function aMove() {
+    var elem = document.getElementsByTagName("a"); 
+    var posY = 10;
+    var id = setInterval(frame, 1);
+	var moving = true;
+    function frame() {
+		if (moving) {
+			posY += 0.1;
+			if (posY >= 14) moving = false;
+		}
+		else {
+			posY -= 0.05;
+			if (posY <= 6) moving = true;
+		}
+		
+		for (var i = 0; i < elem.length; i++) {
+			elem[i].style.top = posY + 'px';
+			//elem[i].style.paddingTop = Math.random() * 50 + "px";
+		}
+		//clearInterval(id);
+    }
+}
---
+++ js/sketches/5/sketch.js
@@ -0,0 +1,70 @@
+var h = 0;
+
+function Line(x1, y1, x2, y2) {
+	this.first = createVector(x1, y1);
+	this.second = createVector(x2, y2);
+	
+	this.update = function() {
+		this.second.x = mouseX;
+		this.second.y = mouseY;
+	}
+	
+	this.show = function() {
+		var x = map(mouseX, 0, width, 0, 1);
+		var y = map(mouseY, 0, height, 0, 1);
+		h = x * y / 0.5;
+		stroke(h, random(0.7, 1), random(0.2, 0.6), random(0.1, 0.3));
+		line(this.first.x, this.first.y, this.second.x, this.second.y);
+	}
+}
+
+var lines = []
+var step = 15;
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	colorMode(HSB, 1.0);
+	
+	// Rangée du haut et du bas
+	for (var x = 0; x < width / step; x++) {
+		lines.push(new Line(x * step, 0, width / 2, height / 2));		// haut
+		lines.push(new Line(x * step, height, width / 2, height / 2));	// bas
+	}
+	// " de droite et de gauche
+	for (var y = 0; y < height / step; y++) {
+		lines.push(new Line(0, y * step, width / 2, height / 2));	// gauche
+		lines.push(new Line(width, y * step, width / 2, height / 2));	// droite
+	}
+}
+
+function mouseMoved() {
+	for (var i = 0; i < lines.length; i++) {
+		lines[i].update();
+	}
+}
+
+var bg = 0;
+var sw = 1;
+var growing = true;
+
+function draw() {
+	background(bg, 0.8, 0.25, 0.6);
+	strokeWeight(sw);
+	
+	bg = h * 0.5;
+	
+	if (growing) {
+		sw += 0.75;
+		if (sw >= 30) growing = false;
+	}
+	else {
+		sw -= 1.5;
+		if (sw <= 3) growing = true;
+	}
+	
+	
+	
+	for (var i = 0; i < lines.length; i++) {
+		lines[i].show();
+	}
+}
